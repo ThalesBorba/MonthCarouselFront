a26c1013f9c576d580dde5a2ee8ac96656fc5cba
ThalesBorba <thales_borba@hotmail.com>
2022-09-19 19:38:05 -0300

    Updating Css

--- Meses.js
+++ Meses.js
@@ -14,14 +14,14 @@ $(document).on('click', '.owl-item>div', function() {
   var $speed = 300;  // in ms
   $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
   
-  var months = document.getElementsByClassName("srcUrl");
+  var months = document.getElementsByClassName("month");
 
-  var src = [];
+  var innerHtml = [];
   for (var i = 0; i < months.length; i++) {
-    src.push(months[i].src);
+    innerHtml.push(months[i].innerHTML);
   }
 
-  var monthName = this.children[0].src.split('=');
+  var monthName = this.children[0].innerHtml;
   
 document.getElementById("months").innerHTML=monthName[1];;
 });
